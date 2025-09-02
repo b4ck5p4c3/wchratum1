8b70c281face76c22f21d5ef83892c79c497d04a
Leonid Evdokimov <leon@darkk.net.ru>
2025-09-02 23:26:28 +0300

    Use OpenBSD headers to get well-known packet struct definitions

--- src/ethaddr.c
+++ src/ethaddr.c
@@ -1,8 +1,13 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2025 Egor Koleda, Leonid Evdokimov. All Rights Reserved.
+ */
 #include "ethaddr.h"
 #include "esig.h"
 #include "chaskey.h"
+#include <string.h>
 
-void MacAddressInitialize(uint8_t mac[ETH_ALEN], unsigned index) {
+void MacAddressInitialize(uint8_t mac[ETHER_ADDR_LEN], unsigned index) {
   // "EthernetMAC0Addr" - context for MAC address generation.
   const uint32_t C_ETHE = UINT32_C(0x45746865);
   const uint32_t C_RNET = UINT32_C(0x726e6574);
@@ -16,7 +21,7 @@ void MacAddressInitialize(uint8_t mac[ETH_ALEN], unsigned index) {
   const uint32_t C_TE_K = UINT32_C(0x6b206574);
 
   // WCHNET_GetMacAddr() sets MAC=(uint8_t*)(ROM_CFG_USERADR_ID+5) and leaks board ID to the wire.
-  // Let's rather use something like KDF.
+  // Let's rather use something like KDF despite weak 64-bit key.
   uint32_t ctx[4] = {C_ETHE, C_RNET, C_MAC0 + index, C_ADDR};
 
   // The first word of key has only few bits of entopy.
@@ -34,5 +39,5 @@ void MacAddressInitialize(uint8_t mac[ETH_ALEN], unsigned index) {
   ctx[1] ^= ctx[3];
 
   memcpy(mac, ctx, 6);
-  mac[0] = (ctx[0] & 0xF0) | 0x02;  // Unicast, Administratively assigned
+  mac[0] = (mac[0] & 0xF0) | 0x02;  // Unicast, Locally administered, Administratively assigned
 }
--- src/ethaddr.h
+++ src/ethaddr.h
@@ -1,11 +1,18 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2025 Egor Koleda, Leonid Evdokimov. All Rights Reserved.
+ */
 #ifndef UUID_144A7A95_2BD3_4246_9062_E04F93608132
 #define UUID_144A7A95_2BD3_4246_9062_E04F93608132
 
-#include <stdint.h>
-
-#define ETH_ALEN 6
+#include <sys/types.h>
+// toolchain-riscv/riscv-wch-elf/include/sys/types.h declares in_addr_t and in_port_t
+#define _IN_TYPES_DEFINED_ 1
+#include <net/if_arp.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
 
 // Use index=0 for R32_ETH_MACA0, index=1 for R32_ETH_MACA1 and so on.
-void MacAddressInitialize(uint8_t mac[ETH_ALEN], unsigned index);
+void MacAddressInitialize(uint8_t mac[ETHER_ADDR_LEN], unsigned index);
 
 #endif // 144A7A95_2BD3_4246_9062_E04F93608132
